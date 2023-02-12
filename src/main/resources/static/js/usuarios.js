f59fbd99f9cfc1e72abe9afe379cab9d5019d77c
SeleneInmaculada <selene.inmaculada@gmail.com>
2023-02-12 18:10:57 +0100

    Hasta el minuto 1:41:00 modificamos usuarios.js para que coja datos del backend mediante fetch y devuelve datos sin pasar por la base

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,6 +1,22 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-
-alert(223455);
+cargarUsuarios();
   $('#usuarios').DataTable();
 });
+
+async function cargarUsuarios(){
+
+
+  const request = await fetch('usuario/2343', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+  });
+  const usuarios = await request.json();
+
+  console.log(usuarios);
+
+
+}
