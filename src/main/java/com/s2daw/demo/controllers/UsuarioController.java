63bd64eaf9279e950ab43d1d4b3a5a598edfc957
SeleneInmaculada <selene.inmaculada@gmail.com>
2023-02-12 19:02:05 +0100

    Hasta el minuto 1:55:00 el back devuelve la lista de los usuarios y el front los pone en una tabla (SIN ACCEDER A LA BD)

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -19,6 +20,36 @@ public class UsuarioController {
         usuario.setEmail("soyunaprueba@gmail.com");
         usuario.setTelefono("666666666");
         return usuario;
+    }
+
+    @RequestMapping(value="usuarios")
+    public List<Usuario> getUsuarios(){
+        List <Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setId(1234L);
+        usuario.setNombre("Hina");
+        usuario.setApellido("Aoi");
+        usuario.setEmail("soyunaprueba@gmail.com");
+        usuario.setTelefono("666666666");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(5678L);
+        usuario2.setNombre("Yuta");
+        usuario2.setApellido("Aoi");
+        usuario2.setEmail("soyunaprueba2222@gmail.com");
+        usuario2.setTelefono("222222222");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(9012L);
+        usuario3.setNombre("Rei");
+        usuario3.setApellido("Sakuma");
+        usuario3.setEmail("soyunaprueba9999@gmail.com");
+        usuario3.setTelefono("999999999");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        return usuarios;
     }
     @RequestMapping(value="usuario6788")
     public Usuario editar(){
