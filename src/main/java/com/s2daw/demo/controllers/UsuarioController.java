52568c9925a6ad8ba7dd60c252f2d44d67601b9d
SeleneInmaculada <selene.inmaculada@gmail.com>
2023-02-12 17:10:53 +0100

    Hasta el minuto 1:26:00 creamos Usuario y nos devuelve un JSON sin acceder a la base de datos

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,13 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="prueba")
-    public List<String> prueba(){
-        return List.of("prueba","prueba2","prueba3");
+    @RequestMapping(value="usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Hina");
+        usuario.setApellido("Aoi");
+        usuario.setEmail("soyunaprueba@gmail.com");
+        usuario.setTelefono("666666666");
+        return usuario;
     }
 }
