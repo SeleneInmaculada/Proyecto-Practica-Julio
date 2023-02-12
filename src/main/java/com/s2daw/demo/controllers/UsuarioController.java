3897231ef93db12cfa8c7c11d99429931ce58b7a
SeleneInmaculada <selene.inmaculada@gmail.com>
2023-02-12 17:24:45 +0100

    Hasta el minuto 1:26:00 Seteamos el ID automaticamente

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.s2daw.demo.controllers;
 
 import com.s2daw.demo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +10,38 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="usuario")
-    public Usuario getUsuario(){
+    @RequestMapping(value="usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Hina");
+        usuario.setApellido("Aoi");
+        usuario.setEmail("soyunaprueba@gmail.com");
+        usuario.setTelefono("666666666");
+        return usuario;
+    }
+    @RequestMapping(value="usuario6788")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Hina");
+        usuario.setApellido("Aoi");
+        usuario.setEmail("soyunaprueba@gmail.com");
+        usuario.setTelefono("666666666");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario78900")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Hina");
+        usuario.setApellido("Aoi");
+        usuario.setEmail("soyunaprueba@gmail.com");
+        usuario.setTelefono("666666666");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario344455")
+    public Usuario buscar(){
         Usuario usuario = new Usuario();
         usuario.setNombre("Hina");
         usuario.setApellido("Aoi");
--- src/main/java/com/s2daw/demo/models/Usuario.java
+++ src/main/java/com/s2daw/demo/models/Usuario.java
@@ -1,6 +1,7 @@
 package com.s2daw.demo.models;
 
 public class Usuario {
+    private Long id;
     private String nombre;
     private String apellido;
     private String email;
@@ -9,6 +10,14 @@ public class Usuario {
 
     public String getNombre() {
         return nombre;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setNombre(String nombre) {
